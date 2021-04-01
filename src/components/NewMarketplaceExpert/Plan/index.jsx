76c44eb08623da8652aea2d121be70da2040b55e
sagarninave <sagar.ninave@konverge.ai>
2021-04-01 15:36:18 +0530

    refresh code in market expert

--- src/components/NewMarketplaceExpert/Plan/index.jsx
+++ src/components/NewMarketplaceExpert/Plan/index.jsx
@@ -24,45 +24,56 @@ function index() {
         <img src={ExpertFour} className="imagesFour" alt="Expert"></img>
       </div>
 
+      {/* Plan Cards */}
+      
       <div className="pt-0 flex lg:mb-0 mb-12 justify-between">
-        
-        <div className="flex flex-col items-center content-center">
+
+        {/* Plan Cards 1 */}
+
+        <div className="flex-1 items-center content-center">
           <div className="boxAnalytics2">
             <p className="marketplace-card-title text-lg md:text-2xl font-bold text-center leading-10" 
                style={{borderBottom: '1px solid #B1B5B9', 
                marginBottom: '15px', 
                padding: '9px 0px'
-               }}>
-                 Plan - 1
-            </p>
+               }}
+            > Plan - 1 </p>
             <ul>
-              <li className="marketplace-card-title font14 text-center text-lg light_grey grey-text font-medium">  Get started for</li>
-              <li className="font14 text-center font-semibold mt-4"> <span>$</span><span className="bigName">12</span></li>
-              <div className="p-2 pl-2 mt-32 pt-0 rounded-md border-2 ml-16 mr-16 mgTB10" style={{width:"11.4rem",margin: 'auto',}}>
+              <li className="marketplace-card-title font14 text-center text-lg light_grey grey-text font-medium">  
+                Get started for
+              </li>
+              <li className="font14 text-center font-semibold mt-4"> 
+                <span>$</span>
+                <span className="bigName">12</span>
+              </li>
+              <div className="p-2 pl-2 mt-32 pt-0 rounded-md border-2 ml-16 mr-16 mgTB10" 
+                   style={{width:"11.4rem",margin: 'auto'}}>
                 <button type="submit" className="text-white purple lg:w-40 w-full flex items-center mt-2 mr-2 justify-between border-none focus:outline-none py-2 px-7 rounded">
-                    <p className="relative" style={{margin:'auto'}}>Make Enquiry</p> 
+                  <p className="relative" style={{margin:'auto'}}>Make Enquiry</p> 
                 </button>
               </div>
             </ul>
             <ul>
               <li className="font14">
-                <span className="tick"></span> Self-Service Audit Document</li>
-              <li className="font14">
-                <p>
+                <p className="financeCardDesc">
                   <span className="tick"></span>
-                  One Hour Additional Legal Advice 
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Credit
+                  Self-Service Audit Document
                 </p>
               </li>
               <li className="font14">
-                <p className="light_grey grey-text">
+                <p className="financeCardDesc">
                   <span className="tick"></span>
-                  30-Minute Audit Review &  <br/>
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Legal Advice
+                  One Hour Additional Legal Advice Credit
+                </p>
+              </li>
+              <li className="font14">
+                <p className="financeCardDesc light_grey grey-text">
+                  <span className="tick"></span>
+                  30-Minute Audit Review & Legal Advice
               </p>
               </li>
               <li className="font14">
-                <p className="light_grey grey-text">
+                <p className="financeCardDesc light_grey grey-text">
                   <span className="tick"></span>
                   Training Video Access Include
                 </p>
@@ -70,95 +81,112 @@ function index() {
             </ul>
           </div>
         </div>
-        <div className="flex flex-col items-center content-center">
+
+        {/* Plan Cards 2 */}
+
+        <div className="flex-1 items-center content-center">
           <div className="boxAnalytics2 text-white" style={{ background: '#38369a'}}>
             <p className="marketplace-card-title text-lg md:text-2xl font-bold text-center leading-10" 
                style={{borderBottom: '1px solid #B1B5B9', 
                marginBottom: '15px', 
                padding: '9px 0px'
-               }}>
-                 Plan - 2
-            </p>            
+               }}
+            > Plan - 2 </p>            
             <ul>
-              <li className="marketplace-card-title font14 text-center text-lg font-medium">  Get started for</li>
-              <li className="font14 text-center font-semibold mt-4"> <span>$</span><span className="bigName">24</span></li>
-              <div className="p-2 pl-2 mt-32 pt-0 rounded-md border-2 ml-16 mr-16 mgTB10" style={{width:"11.4rem",margin: 'auto',}}>
+              <li className="marketplace-card-title font14 text-center text-lg font-medium">
+                Get started for
+              </li>
+              <li className="font14 text-center font-semibold mt-4">
+                <span>$</span>
+                <span className="bigName">24</span>
+              </li>
+              <div className="p-2 pl-2 mt-32 pt-0 rounded-md border-2 ml-16 mr-16 mgTB10" 
+                   style={{width:"11.4rem",margin: 'auto'}}>
                 <button type="submit" className="text-white whiteBg lg:w-40 w-full flex items-center mt-2 mr-2 justify-between border-none focus:outline-none py-2 px-7 rounded">
-                    <p className="relative" style={{margin:'auto'}}>Make Enquiry</p> 
+                  <p className="relative" style={{margin:'auto'}}>
+                    Make Enquiry
+                  </p> 
                 </button>
               </div>
             </ul>
             <ul>
               <li className="font14">
-                <span className="tick" style={{
-                borderBottom: ' 3px solid #fff',
-                borderRight: '3px solid #fff'
-              }}></span> Self-Service Audit Document</li>
+                <p className="financeCardDesc">
+                  <span className="tick activeCard"></span> Self-Service Audit Document
+              </p></li>
               <li className="font14">
-                <p>
-                  <span className="tick" style={{
-                borderBottom: ' 3px solid #fff',
-                borderRight: '3px solid #fff'
-              }}></span>
-                  One Hour Additional Legal Advice 
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Credit
+                <p className="financeCardDesc">
+                  <span className="tick activeCard"></span>
+                  One Hour Additional Legal Advice Credit
                 </p>
               </li>
               <li className="font14">
-                <p>
-                  <span className="tick" style={{
-                borderBottom: ' 3px solid #fff',
-                borderRight: '3px solid #fff'
-              }}></span>
-                  30-Minute Audit Review &  <br/>
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Legal Advice
+                <p className="financeCardDesc">
+                  <span className="tick activeCard"></span>
+                  30-Minute Audit Review & Legal Advice
               </p>
               </li>
               <li className="font14">
 
-              <p className="light_grey grey-text">
-                <span className="tick" style={{
-                borderBottom: ' 3px solid #fff',
-                borderRight: '3px solid #fff'
-              }}></span>
+              <p className="financeCardDesc light_grey grey-text">
+                <span className="tick activeCard"></span>
                   Training Video Access Include
               </p>
               </li>
             </ul>
           </div>
         </div>
-        <div className="flex flex-col items-center content-center">
+
+      {/* Plan Cards 3 */}
+
+        <div className="flex-1 items-center content-center">
           <div className="boxAnalytics2">
-            <p className="marketplace-card-title text-lg md:text-2xl font-bold leading-10" style={{ borderBottom: '1px solid #B1B5B9', marginBottom: '15px', padding: '9px 0px', textAlign: 'center' }}>Plan - 3</p>
+            <p className="marketplace-card-title text-lg md:text-2xl font-bold leading-10" 
+              style={{
+                borderBottom: '1px solid #B1B5B9', 
+                marginBottom: '15px', 
+                padding: '9px 0px', 
+                textAlign: 'center' 
+              }}
+            > Plan - 3</p>
             <ul>
-              <li className="marketplace-card-title font14 text-center text-lg light_grey grey-text font-medium">  Get started for</li>
-              <li className="font14 text-center font-semibold mt-4"> <span>$</span><span className="bigName">36</span></li>
-              {/* <li className="font14 makeEnquery rounded-sm">Make Enquiry</li> */}
-              <div className="p-2 pl-2 mt-32 pt-0 rounded-md border-2 ml-16 mr-16 mgTB10" style={{width:"11.4rem",margin: 'auto',}}>
+              <li className="marketplace-card-title font14 text-center text-lg light_grey grey-text font-medium">  
+                Get started for
+              </li>
+              <li className="font14 text-center font-semibold mt-4"> 
+                <span>$</span>
+                <span className="bigName">36</span>
+              </li>
+              <div className="p-2 pl-2 mt-32 pt-0 rounded-md border-2 ml-16 mr-16 mgTB10" 
+                   style={{width:"11.4rem",margin: 'auto',}}>
                 <button type="submit" className="text-white purple lg:w-40 w-full flex items-center mt-2 mr-2 justify-between border-none focus:outline-none py-2 px-7 rounded">
-                    <p className="relative" style={{margin:'auto'}}>Make Enquiry</p> 
+                  <p className="relative" style={{margin:'auto'}}>
+                    Make Enquiry
+                  </p> 
                 </button>
               </div>
             </ul>
             <ul>
               <li className="font14">
-                <span className="tick"></span> Self-Service Audit Document</li>
-              <li className="font14">
-                <p>
-                  <span className="tick"></span>
-                  One Hour Additional Legal Advice 
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Credit
+                <p className="financeCardDesc">
+                  <span className="tick"></span> 
+                  Self-Service Audit Document
                 </p>
               </li>
               <li className="font14">
-                <p>
+                <p className="financeCardDesc">
                   <span className="tick"></span>
-                  30-Minute Audit Review &  <br/>
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Legal Advice
-              </p>
+                  One Hour Additional Legal Advice Credit
+                </p>
               </li>
               <li className="font14">
-                <p>
+                <p className="financeCardDesc">
+                  <span className="tick"></span>
+                  30-Minute Audit Review & Legal Advice
+                </p>
+              </li>
+              <li className="font14">
+                <p className="financeCardDesc">
                   <span className="tick"></span>
                   Training Video Access Include
                 </p>
